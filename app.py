ea4cd9cfe171fed4e02291931510f9960e9446e3
ahan-strak <ahanabubakar@gmail.com>
2021-12-01 23:11:46 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
+
+cnx = mysql.connector.connect(
+    user='root', password='1234', host='localhost', database='dbms')
+cursor = cnx.cursor()
+app = Flask(__name__)
+
+
+@app.route('/rooms', methods=['POST'])
+def save_task():
+    room_id = request.form.get('room_id')
+    cost_room=request.form.get('cost_room')
+    query = ("insert into rooms values({}, {})".format(room_id, cost_room))
+    cursor.execute(query)
+    cnx.commit()
+    return redirect(url_for('get_rooms'))
+
+@app.route('/rooms/insert')
+def get_rooms():
+    return render_template('index.html')
+
+@app.route('/rooms/display')
+def dis_rooms():
+    rooms=[]
+    query=("SELECT room_no,room_cost FROM rooms ")
+    cursor.execute(query)
+    for (room_no,room_cost) in cursor:
+        rooms.append('{} {}'.format(room_no,room_cost))
+        return render_template('dis_room.html',disprooms=rooms)
+
+
---
+++ dbms_db.sql
@@ -0,0 +1,2 @@
+USE dbms;
+CREATE TABLE rooms(room_no INT(4),room_cost INT(5));
